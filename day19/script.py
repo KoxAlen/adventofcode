031f2b9e391e6dd85ec85b6fa25dbb1f8a1bb524
Rafael Blazquez (KoxAlen) <KoxAlen@gmail.com>
2015-12-20 10:41:07 +0100

    Changed it to randomize the lookup table, was able to solve some of the "problem" inputs on reddit
    Added the reverse enginnered version of Askalski, just because I found it interesting

--- day19/script.py
+++ day19/script.py
@@ -1,5 +1,5 @@
 #Written for Python 3.4.2
-from collections import defaultdict
+import random
 
 replacements = dict()
 with open("input.txt", "r") as f:
@@ -35,17 +35,47 @@ def lookup(grammar):
         for t in grammar[k]:
             result[t] = k
     return result
+
 def reduce(grammar, start, string):
     r_lookup = lookup(grammar)
-    t = sorted(r_lookup.keys(), key=lambda n: len(n), reverse=True)
+    t = list(r_lookup.keys())
     trans = []
-    while string != start:
+    tmp = string
+    while tmp != start:
+        prev = tmp
         for h in t:
-            if string == start:
+            if tmp == start:
                 break
-            if h in string:
+            if h in tmp:
                 trans.append(r_lookup[h])
-                string = string.replace(h, r_lookup[h], 1)
+                tmp = tmp.replace(h, r_lookup[h], 1)
+        if prev == tmp:
+            print("Stuck on %s" % tmp)
+            random.shuffle(t)
+            tmp = string
+            trans = []
+            print("Randomize and retry")
     return list(reversed(trans))
+
+def askalski_solution(string):
+    """
+    Solution based on the properties of the grammar
+    https://www.reddit.com/r/adventofcode/comments/3xflz8/day_19_solutions/cy4etju
+    Even though my solution works (at least for my input) I found this interesting"""
+    t = 0
+    i = 0
+    while i < len(string):
+        try:
+            if string[i+1].islower():
+                i += 1
+        except IndexError:
+            pass
+        i += 1
+        t += 1
+    par = string.count("Rn") + string.count("Ar")
+    sep = string.count("Y")
+    return t - par - sep*2 -1
+
+print("Askalski solution: %d" % askalski_solution(target))
 
 print(len(reduce(replacements, "e", target)))
