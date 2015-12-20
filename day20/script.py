472fbcd4aa0c8b79e803304969e47b18a07cf326
Rafael Blazquez (KoxAlen) <KoxAlen@gmail.com>
2015-12-20 12:15:58 +0100

    I like this more than the generator, idk

--- day20/script.py
+++ day20/script.py
@@ -8,20 +8,18 @@ puzzle_input = 33100000
 def integer_factorization(n):
     return set(functools.reduce(list.__iadd__, ([i, n//i] for i in range(1, int(sqrt(n))+1) if n % i == 0)))
 
-def elfHouses():
-    for i in itertools.count(start=1):
-        elfs = integer_factorization(i)
-        presents = sum(elfs)*10
-        lazy_presents = sum([n for n in elfs if i <= n*50])*11
-        yield i, presents, lazy_presents
-
 part1 = False
 part2 = False
-for house, presents, lazy_presents in elfHouses():
-    if not part1 and presents >= puzzle_input:
-        print("Part 1", house, presents)
-        part1 = True
-    if not part2 and lazy_presents >= puzzle_input:
-        print("Part 2", house, lazy_presents)
-        part2 = True
+for house in itertools.count(start=1):
+    elfs = integer_factorization(house)
+    if not part1:
+        presents = sum(elfs)*10
+        if presents >= puzzle_input:
+            print("Part 1", house, presents)
+            part1 = True
+    if not part2:
+        lazy_presents = sum([n for n in elfs if house <= n*50])*11
+        if lazy_presents >= puzzle_input:
+            print("Part 2", house, lazy_presents)
+            part2 = True
     if part1 and part2: break
