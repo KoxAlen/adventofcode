1ae8730c5633521d7df722302db2bf909d6215ad
Rafael Blazquez (KoxAlen) <KoxAlen@gmail.com>
2015-12-28 18:51:04 +0100

    Made it easier to understand in a future

--- day25/script.py
+++ day25/script.py
@@ -1,9 +1,11 @@
 #Written for Python 3.4.2
 
 def index(row, col):
-    return (row+col-2)*(row+col-1)//2+col
+    N = row+col - 2
+    Tn = (N*(N+1))//2 #Triangular number
+    return Tn + col-1 
 
 def calculate(seed, row, col, base=252533, modulo=33554393):
-    return pow(base, index(row, col)-1, modulo)*seed % modulo
+    return pow(base, index(row, col), modulo)*seed % modulo
 seed = 20151125
 print(calculate(seed, 2981, 3075)) #Input row=2981, col=3075
